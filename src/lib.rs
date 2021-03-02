40a5abb8c413dbd3ae6974a9036f7346b61b2507
Eric Rochester <eric.rochester@willowtreeapps.com>
2021-03-02 15:58:19 -0500

    stubbing in stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,18 @@
-#![recursion_limit="128"]
+#![recursion_limit="512"]
 
-extern crate strum;
-#[macro_use]
-extern crate strum_macros;
-#[macro_use]
-extern crate yew;
-
+use log::info;
 use strum::IntoEnumIterator;
-use yew::html::*;
+use yew::prelude::*;
 use yew::services::console::ConsoleService;
+use wasm_bindgen::prelude::*;
+use wasm_logger;
 
 struct Context {
     console: ConsoleService,
 }
 
 struct Model {
+    link: ComponentLink<Self>,
     notes: Vec<Note>,
     new_title: String,
     new_text: String,
@@ -31,65 +29,88 @@ enum Msg {
     NoOp,
 }
 
-fn update(context: &mut Context, input: &mut Model, msg: Msg) {
-    match msg {
-        Msg::AddNote => {
-            let console = &context.console;
-            console.log(&format!("new_title = {:?}", &input.new_title));
-            console.log(&format!("new_text  = {:?}", &input.new_text));
-        },
-        _ => {
-        },
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            notes: Vec::new(),
+            new_text: String::new(),
+            new_title: String::new(),
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddNote => {
+                info!("new-title: {:?}", &self.new_title);
+                info!("new-text: {:?}", &self.new_text);
+            },
+            _ => {},
+        };
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        Viewable::view(self)
     }
 }
 
 trait Viewable {
-    fn view(&self) -> Html<Msg>;
+    fn view(&self) -> Html;
 }
 
 impl Viewable for Model {
-    fn view(&self) -> Html<Msg> {
+    fn view(&self) -> Html {
         html! {
-            <div class="container",>
-                { for self.notes.iter().map(Note::view) }
-                <div class="fixed-action-btn",>
-                    <a class=("btn-floating", "btn-large", "waves-effect", "waves-light", "modal-trigger"), href="#add-modal",>
-                        <i class=("large", "material-icons"),>{ "add" }</i>
-                    </a>
-                </div>
-            </div>
-            <div id="add-modal", class="modal",>
-                <div class="modal-content",>
-                    <h4>{ "Add Note" }</h4>
-                    <div class="row",>
-                        <form class=("col", "s12"),>
-                            <div class="row",>
-                                <div class=("input-field", "col", "s12"),>
-                                    <input placeholder="title", id="title_input", type="text", value=&self.new_title, ></input>
-                                    <label for="title_input",>{ "Title" }</label>
-                                </div>
-                            </div>
-                            <div class="row",>
-                                <div class=("input-field", "col", "s12"),>
-                                    <textarea id="body_input", class="materialize-textarea",>{ &self.new_text }</textarea>
-                                    <label for="body_input",>{ "Body" }</label>
-                                </div>
-                            </div>
-                        </form>
+            <>
+                <div class="container",>
+                    { for self.notes.iter().map(Note::view) }
+                    <div class="fixed-action-btn",>
+                        <a class=("btn-floating", "btn-large", "waves-effect", "waves-light", "notes-trigger"), href="#add-modal",>
+                            <i class=("large", "material-icons"),>{ "add" }</i>
+                        </a>
                     </div>
+                </div>
+                <div id="add-modal", class="modal",>
+                    <div class="modal-content",>
+                        <h4>{ "Add Note" }</h4>
+                        <div class="row",>
+                            <form class=("col", "s12"),>
+                                <div class="row",>
+                                    <div class=("input-field", "col", "s12"),>
+                                        <input placeholder="title", id="title_input", type="text", value=&self.new_title, />
+                                        <label for="title_input",>{ "Title" }</label>
+                                    </div>
+                                </div>
+                                <div class="row",>
+                                    <div class=("input-field", "col", "s12"),>
+                                        <textarea id="body_input", class="materialize-textarea",>{ &self.new_text }</textarea>
+                                        <label for="body_input",>{ "Body" }</label>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
 
+                    </div>
+                    <div class="modal-footer",>
+                        <button class=("modal-action", "modal-close", "waves-effect", "waves-green", "btn-flat"), onclick=self.link.callback(|_| Msg::AddNote)>{ "Add" }</button>
+                        <button class=("modal-action", "modal-close", "btn-flat"), onclick=self.link.callback(|_| Msg::CancelAdd)>{ "Cancel" }</button>
+                    </div>
                 </div>
-                <div class="modal-footer",>
-                    <button class=("modal-action", "modal-close", "waves-effect", "waves-green", "btn-flat"), onclick=|_| Msg::AddNote,>{ "Add" }</button>
-                    <button class=("modal-action", "modal-close", "btn-flat"), onclick=|_| Msg::CancelAdd,>{ "Cancel" }</button>
-                </div>
-            </div>
+            </>
         }
     }
 }
 
 impl Viewable for Note {
-    fn view(&self) -> Html<Msg> {
+    fn view(&self) -> Html {
         html! {
             <div class="row",>
                 <div class=("col", "s12", "m6"),>
@@ -105,25 +126,8 @@ impl Viewable for Note {
     }
 }
 
-fn main() {
-    let mut app = App::new();
-    let context = Context {
-        console: ConsoleService,
-    };
-    let state = Model {
-        notes: vec![
-            Note {
-                title: String::from("Everything Here is Beautiful"),
-                text: String::from("by Mira T. Lee"),
-            },
-            Note {
-                title: String::from("The Friend"),
-                text: String::from("by Sigrid Nunez"),
-            }
-        ],
-        new_title: String::from(""),
-        new_text: String::from(""),
-    };
-
-    app.run(context, state, update, Model::view);
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    wasm_logger::init(wasm_logger::Config::default());
+    App::<Model>::new().mount_as_body();
 }
